040b12cc6ff8fa91318af4245fbd7034213e6750
Marcos <marcos@rookbirdgames.com>
2018-10-27 00:18:20 -0300

    git add -A
    git add -A

---
+++ Unity/The First Sinner/Assets/Scripts/UI/DemonData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class DemonData : MonoBehaviour
+{
+
+
+	[Range(0,1)][SerializeField]
+	private float TesterRange;
+	
+	
+	
+	/// <summary>
+	/// Contract
+	/// 	return => Range(0,1)
+	/// </summary>
+	public float Health
+	{
+		get
+		{
+			return TesterRange;
+		}
+		set
+		{
+			TesterRange = value;
+		}
+	}
+	
+	
+}
---
+++ Unity/The First Sinner/Assets/Scripts/UI/ObserverDemonData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+	[RequireComponent(typeof(Image))]
+	public class ObserverDemonData : MonoBehaviour
+	{
+
+		private Image Image => GetComponent<Image>();
+
+
+		public DemonData Data;
+
+
+		private void Update()
+		{
+			Image.fillAmount = Data.Health;
+		}
+	}
+}
+
