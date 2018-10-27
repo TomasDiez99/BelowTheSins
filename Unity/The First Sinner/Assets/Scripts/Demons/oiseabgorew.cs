1cb3b8b2c1c915abe9781cb4c6ecddd7aa454e19
Marcos <marcos@rookbirdgames.com>
2018-10-27 00:31:15 -0300

    demons

---
+++ Unity/The First Sinner/Assets/Scripts/Demons/oiseabgorew.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Patterns;
+using UnityEngine;
+
+public interface IDemon
+{
+    float Health { get; }
+}
+
+
+
+public interface IPhrase
+{
+    string Demon { get; }
+    string Content { get; }
+    List<IDemon> SinsRelated { get; }
+}
+/// <summary>
+/// Contents all demons (Statically)
+/// </summary>
+public class DemonManager : ScriptSingleton<DemonManager>
+{
+    public IDemon Gula { get; } = new PlaceHolderDemon();
+    public IDemon Envidia { get; } = new PlaceHolderDemon();
+    public IDemon Lujuria { get; } = new PlaceHolderDemon();
+    public IDemon Soberbia { get; } = new PlaceHolderDemon();
+    public IDemon Ira { get; } = new PlaceHolderDemon();
+    public IDemon Pereza { get; } = new PlaceHolderDemon();
+    public IDemon Codicia { get; } = new PlaceHolderDemon();
+}
+
+public class PlaceHolderDemon : IDemon
+{
+    public float Health { get; }
+}
