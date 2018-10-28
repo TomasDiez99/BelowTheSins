4bdcfb2780889db8267b0859e3920c5d7113d2c9
emrod <emrodk120@gmail.com>
2018-10-28 01:22:40 -0300

    Music in assets

--- Unity/The First Sinner/Assets/GameItself.cs
+++ Unity/The First Sinner/Assets/GameItself.cs
@@ -14,10 +14,11 @@ public class GameItself : ScriptSingleton<GameItself>
 
 	public DialogWriter ToWrite => FindObjectOfType<DialogWriter>();
 	private AbstractDiscreteInput tecla,right,left;
-	private int index = 4;
+	private int index = 3;
 	private const int minCapital=0;
 	private const int maxCapital = 6;
 	private IDemonParsed demon;
+	private Color colorPrevius;
 	
 	
 	private void Start()
@@ -29,24 +30,31 @@ public class GameItself : ScriptSingleton<GameItself>
 		right.Event += SelectRight;
 		left.Event += SelectLeft;
 		tecla.Event += BeginGame;
-		Select();
+		Selected();
 		
 	}
 
-	private void Select()
+	private void Selected()
 	{
+		if (colorPrevius != null && demon != null)
+		{
+			demon.Halo.DOColor(colorPrevius, 1);
+		} 
 		demon=DemonManager.Instance.GetDemon(index);
+		colorPrevius = demon.Halo.color;
+		var color =demon.Halo.DOColor(Color.black, 1);
 	}
 
 	private void SelectLeft()
 	{
 		index = index == minCapital ? maxCapital : index - 1;
-		
+		Selected();
 	}
 
 	private void SelectRight()
 	{
 		index = index == maxCapital ? minCapital : index + 1;
+		Selected();
 	}
 
 	public void BeginGame()
